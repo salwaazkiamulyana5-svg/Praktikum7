218a73efcda76ef59066713e183ff6f2394133c2
salwaazkiamulyana5-svg <salwaazkiamulyana5@gmail.com>
2025-11-09 19:50:26 +0700

    Delete praktikum3 directory

--- praktikum3/laravel/resources/views/about.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>About Page</h1>
-<p>Nama: {{ $name }}</p>
-
-<a href="{{ route('home.page') }}">Kembali ke Home</a>
--- praktikum3/modul3/resources/views/components/alert.blade.php
+++
@@ -1,3 +0,0 @@
-<div style="padding:10px; border:1px solid {{ $type === 'eror' ? 'red' : 'green' }}; margin:10px 0;">
-    <strong>{{ ucfirst($type) }}:</strong> {{ $message}}
-</div>
